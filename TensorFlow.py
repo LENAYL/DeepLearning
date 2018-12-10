0de3e59bb68544e1ed95c04fbf41cbe2009ebb8d
LENAYL <yanlina94@163.com>
2018-12-10 20:17:02 +0800

    Initial commit

---
+++ TensorFlow.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+import os
+import time
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+# start = time.time()
+# a = tf.random_normal((100, 100))
+# b = tf.random_normal((100, 500))
+# c = tf.matmul(a, b)
+# sess = tf.InteractiveSession()
+# end = time.time()
+# print((end - start) * 1000, end='ms')
+import numpy as np
+import matplotlib.pyplot as plt
+
